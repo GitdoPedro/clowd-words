ceb727c54cc228fef329f7383a3028ed7ac64e39
GitdoPedro <emailpedrolima@gmail.com>
2023-08-30 23:20:46 -0300

    lógica

--- server/index.js
+++ server/index.js
@@ -23,6 +23,44 @@ io.on('connection', (socket) => {
   });
 });
 
+/*io.on('connection', async (socket) => {
+  try {
+    // Obter todas as palavras do banco de dados
+    const words = await Word.findAll();
+    
+    // Enviar as palavras para o cliente recém-conectado usando o evento receive_word
+    socket.emit('receive_word', words);
+
+    socket.on('set_word', async (text) => {
+      try {
+        // Emitir o evento para o Socket.IO
+        io.emit('receive_word', {
+          text,
+          textId: socket.id,
+        });
+
+        // Realizar a operação de POST no backend
+        const response = await fetch('/api/word', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ text }), // Envie a palavra para o backend
+        });
+
+        if (!response.ok) {
+          const errorData = await response.json();
+          throw new Error(errorData.error);
+        }
+      } catch (error) {
+        console.error('Erro ao adicionar palavra:', error.message);
+      }
+    });
+  } catch (error) {
+    console.error('Erro ao obter palavras do banco de dados:', error.message);
+  }
+});*/
+
 
 const Word = WordModel(sequelize, sequelize.Sequelize.DataTypes);
 sequelize.sync().then(() => {
